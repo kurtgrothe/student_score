6c47e690cbb2e5a27093c27a9444456310f6bd4a
Kurt V Grothe <kurt.grothe@outlook.com>
2023-01-15 21:01:00 -0800

    config changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 import joblib
 app = Flask(__name__, template_folder='templates', static_folder='static')
-filename = 'brst_cncr_lgreg.pkl'
+filename = 'test_score.pkl'
 #model = pickle.load(open(filename, 'rb'))
 model = joblib.load(filename)
 #model = joblib.load(filename)
@@ -18,23 +18,30 @@ def index():
 
 
 def predict():
-    perimeter_worst = request.form['perimeter_worst']
-    radius_worst  = request.form['radius_worst']
-    concave_points_mean = request.form['concave_points_mean']
-    area_worst = request.form['area_worst']
+    reading_score = request.form['reading_score']
+    writing_score = request.form['writing_score']
+    math_score = request.form['math_score']
 
-    pred = model.predict(np.array([[float(perimeter_worst),float(radius_worst),
-                                    float(concave_points_mean), float(area_worst)]]))
 
-    result = "not valid"                                
-    if pred == 0:
-        result = "No Cancer Found"
-    else:
-        result = "Cancer Present"
+    pred = model.predict(np.array([[reading_score, writing_score,
+                                    math_score]], dtype=float))
+    print(pred)
 
-    return render_template('index.html', predict=result)
 
-    #return render_template('index.html', predict=str(pred))
+    # if pred == 0:
+    #     result = "Group A"
+    # elif pred ==1:
+    #     result = "Group B"
+    # elif pred ==2:
+    #     result = "Group C"
+    # elif pred ==3:
+    #     result = "Group D"
+    # else:
+    #     result = "Group E"
+
+
+    return render_template('index.html', predict=str(pred))
+
 
 
 if __name__ == '__main__':
